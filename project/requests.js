52fa459f9733fd883b7489a91e042747e311c346
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-11-28 05:27:35 +0000

    requisicoes de regiao e de escolas por regiao inseridas

---
+++ project/requests.js
@@ -0,0 +1,48 @@
+let header = {
+    'X-Parse-Application-Id': 'u3w3KPgn5ja6f1i7XW2OVnilH9vceeVtlKvhVv6D',
+    'X-Parse-REST-API-Key': 'QYqdyz2IUhM175KWtEqiDX7adZN9KXUtOSrSp3IA'
+}
+
+let headerJason = {
+    ...header,
+    'content-type': 'application/json'
+}
+
+async function getRegiao() {
+    let regioes = []
+    let dataEscolas = await fetch('https://parseapi.back4app.com/classes/censo_esc_2023',{
+        method: 'GET',
+        headers: header
+    }).then((data)=>{
+        return data.json()
+    })
+    
+    for (const escola of dataEscolas.results) {
+        let nomeRegiao = await escola.NO_REGIAO
+        regioes.push(nomeRegiao)
+    }
+    regioesUnicas = [...new Set(regioes)]
+
+    return regioesUnicas
+}
+
+async function getDataPorRegiao(regiao) {
+
+    let where = new URLSearchParams({
+        where: JSON.stringify({
+            NO_REGIAO: regiao
+        })
+    })
+    console.log(`https://parseapi.back4app.com/classes/censo_esc_2023/${where.toString()}`);
+    
+    let data = await fetch(`https://parseapi.back4app.com/classes/censo_esc_2023?${where.toString()}`,{
+        method: 'GET',
+        headers: headerJason
+    }).then((data)=>{
+        return data.json()
+    })
+
+    console.log(data);
+    return data
+}
+
