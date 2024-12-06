59d3ac44a7961ad57a389340e86d487a4b77fce2
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-12-06 20:50:12 +0000

    listar escolas com backend

---
+++ pags/listarEscolas.js
@@ -0,0 +1,68 @@
+Parse.serverURL = 'https://parseapi.back4app.com'; 
+
+Parse.initialize(
+    'u3w3KPgn5ja6f1i7XW2OVnilH9vceeVtlKvhVv6D',
+    'KmiuyRNhRdaExPIvsucfvc3JUIPD3hoKeHkaCApN' 
+)
+
+window.onload = getSchools(1)
+
+async function getSchools(page) {
+    let listSchools = document.getElementById('schools')
+    listSchools.innerHTML = ''
+
+    let skip = (page - 1) * 20;
+    let limiteDeRegisros = 20;
+
+    const querySchools = new Parse.Query('censo_esc_2023');
+    querySchools.select('IN_INTERNET','QT_PROF_BIBLIOTECARIO','TP_DEPENDENCIA','NO_MESORREGIAO','NO_ENTIDADE', 'NO_REGIAO','iN_BIBLIOTECA');
+    querySchools.limit(limiteDeRegisros);
+    querySchools.skip(skip);
+
+    let dataSchools = await querySchools.find()
+
+    dataSchools.forEach(async (school)=>{
+        console.log(school.get('NO_ENTIDADE'));
+        
+        let container = document.createElement('div')
+        container.classList.add('card')
+        container.classList.add('d-flex')
+        container.classList.add('flex-column')
+        container.classList.add('justify-content-center')
+
+        let nameSchool = document.createElement('div')
+        nameSchool.classList.add('txt')
+        nameSchool.classList.add('d-flex')
+        nameSchool.classList.add('flex-row')
+        nameSchool.classList.add('justify-content-center')
+        let nameSchoolText = document.createElement('p')
+        nameSchoolText.classList.add('card-text')
+        nameSchoolText.textContent = school.get('NO_ENTIDADE')
+
+        let infosSchool = document.createElement('div')
+        infosSchool.classList.add('data')
+        infosSchool.classList.add('d-flex')
+        infosSchool.classList.add('flex-row')
+        infosSchool.classList.add('justify-content-between')
+
+        let regionText = document.createElement('p')
+        regionText.textContent = `Região: ${school.get('NO_REGIAO')}`
+        
+        let mesorregionText = document.createElement('p')
+        mesorregionText.textContent = `Mesorregião: ${school.get('NO_MESORREGIAO')}`
+
+        let acessNet = document.createElement('p')
+        acessNet.textContent = `Acesso à internet: ${school.get('IN_INTERNET') == 1 ? 'Possui' : 'Não possui'}`
+
+        infosSchool.appendChild(regionText)
+        infosSchool.appendChild(mesorregionText)
+        infosSchool.appendChild(acessNet)
+
+        nameSchool.appendChild(nameSchoolText)
+        container.appendChild(nameSchool)
+        container.appendChild(infosSchool)
+        listSchools.appendChild(container)
+    })
+
+    localStorage.setItem('page',page+1)
+}
